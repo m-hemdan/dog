83a7adeb618975cc846215de026649d73348822d
Marwa Hemdan <marwahemdan4410@yahoo.com>
2024-09-28 18:49:53 -0400

    third commit

--- dog.go
+++ dog.go
@@ -4,6 +4,9 @@ import (
 	"strings"
 )
 
-func WhenGrownUp(s string) string {
+func WhenGrownUps(s string) string {
 	return "When the puppy grows up it says: " + strings.ToUpper(s)
+}
+func WhenGrownUp(s string) string {
+	return "When the puppy grows up it says: lowercase " + strings.ToLower(s)
 }
